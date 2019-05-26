ba4298d71112f4a9dfd1a8e58ce9aa91e0689bca
Stella McEwen <mcewen.stella@gmail.com>
2019-05-26 19:03:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,6 @@ class Person
   attr_accessor 
   
   def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}, " ))}
+    attributes.each {|key, value| self.send(("#{key}=" ))}
   end
 end
